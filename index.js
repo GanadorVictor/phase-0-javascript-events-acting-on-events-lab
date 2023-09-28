fa92fbef59a25ecb5ee69cac2079f6b863124ec4
Victor Muturi <muturivictor21@gmail.com>
2023-09-28 16:29:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    // Define the maximum allowed left position (width of the game field minus the dodger's width)
+    const maxWidth = 400 - 40;
+  
+    if (left < maxWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+    
